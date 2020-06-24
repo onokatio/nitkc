43afdaf0a418b91e0b5c1f46ba006ecc6aa5b18d
onokatio <onokatio@gmail.com>
2020-06-24 23:50:12 +0900

    implement fft

--- 5/info-net/baseband/main.py
+++ 5/info-net/baseband/main.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.gridspec as gridspec
 import random
 
 data = [random.randrange(0,2) for i in range(1024)]
@@ -46,6 +47,7 @@ def graphdata(data):
         print(" ", end="")
     print("")
 
+"""
 print(data)
 print(NRZ(data))
 print(NRZ2(data))
@@ -53,7 +55,7 @@ print(RZ(data))
 print(RZ2(data))
 print(AMI(data))
 print(MAN(data))
-
+"""
 
 def plotwave(fig, pos, data, title):
     width = 25
@@ -62,6 +64,7 @@ def plotwave(fig, pos, data, title):
     ax1.set_title(title)
     ax1.set_xticks(np.arange(0, (width+1)*4, 4), minor=False)
     ax1.set_xticks(np.arange(0, (width+1)*4, 1), minor=True)
+    ax1.set_xticklabels(range(width+1))
     ax1.set_axisbelow(False)
     ax1.xaxis.grid(True, 'minor', linestyle='--', zorder=1)
     ax1.xaxis.grid(True, 'major', color='r', zorder=6)
@@ -70,25 +73,28 @@ def plotwave(fig, pos, data, title):
     ax1.set_xlim(0,width*4)
     return ax1
 
+def plotfft(fig, pos, data, title):
+    F = np.abs(np.fft.fft(data.flatten()))
+    ax = fig.add_subplot(pos)
+    ax.plot(range(len(F)), F)
+    ax.set_title(title)
 
 fig = plt.figure(tight_layout=True)
-plotwave(fig, 711, NORMAL(data), 'data')
-plotwave(fig, 712, NRZ(data), 'NRZ')
-plotwave(fig, 713, NRZ2(data), 'NRZ2')
-plotwave(fig, 714, RZ(data), 'RZ')
-plotwave(fig, 715, RZ2(data), 'RZ2')
-plotwave(fig, 716, AMI(data), 'AMI')
-plotwave(fig, 717, MAN(data), 'MAN')
+spec = gridspec.GridSpec(ncols=2, nrows=7, figure=fig)
+plotwave(fig, spec[0,0], NORMAL(data), 'data')
+plotwave(fig, spec[1,0], NRZ(data), 'NRZ')
+plotwave(fig, spec[2,0], NRZ2(data), 'NRZ2')
+plotwave(fig, spec[3,0], RZ(data), 'RZ')
+plotwave(fig, spec[4,0], RZ2(data), 'RZ2')
+plotwave(fig, spec[5,0], AMI(data), 'AMI')
+plotwave(fig, spec[6,0], MAN(data), 'MAN')
+
+plotfft(fig, spec[0,1], NORMAL(data), 'data fft')
+plotfft(fig, spec[1,1], NRZ(data), 'NRZ fft')
+plotfft(fig, spec[2,1], NRZ2(data), 'NRZ2 fft')
+plotfft(fig, spec[3,1], RZ(data), 'RZ fft')
+plotfft(fig, spec[4,1], RZ2(data), 'RZ2 fft')
+plotfft(fig, spec[5,1], AMI(data), 'AMI fft')
+plotfft(fig, spec[6,1], MAN(data), 'MAN fft')
+
 plt.show()
-
-"""
-graphdata(NORMAL(data[1:24]))
-graphdata(NRZ(data[1:24]))
-graphdata(NRZ2(data[1:24]))
-graphdata(RZ(data[1:24]))
-graphdata(RZ2(data[1:24]))
-graphdata(AMI(data[1:24]))
-graphdata(MAN(data[1:24]))
-"""
-
-#print(np.reshape([1,2,3,4], (-1, 1) ) * [2, 3])
