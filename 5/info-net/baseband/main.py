e54245f9c766e7af649620dcd6972d4f3ac74231
onokatio <onokatio@gmail.com>
2020-06-24 21:52:23 +0900

    plot

--- 5/info-net/baseband/main.py
+++ 5/info-net/baseband/main.py
@@ -54,22 +54,41 @@ print(RZ2(data))
 print(AMI(data))
 print(MAN(data))
 
-width = 50
 
-data = data[0:width]
+def plotwave(fig, pos, data):
+    width = 25
+    data = data[0:width]
+    ax1 = fig.add_subplot(pos)
+    ax1.set_xticks(np.arange(0, width * 4, 4), minor=False)
+    ax1.set_xticks(np.arange(0, width * 4, 1), minor=True)
+    ax1.set_axisbelow(False)
+    ax1.xaxis.grid(True, 'minor', linestyle='--', zorder=1)
+    ax1.xaxis.grid(True, 'major', color='r', zorder=6)
+    ax1.plot(range(width * 4), data.flatten(), drawstyle='steps-post')
+    ax1.set_ylim(-1.1,1.1)
+    return ax1
+
 
 fig = plt.figure()
-ax1 = fig.add_subplot(711)
-ax1.plot(range(width * 4), NORMAL(data).flatten())
-ax = fig.add_subplot(712).plot(range(width * 4), NRZ(data).flatten(), drawstyle='steps-pre')
+plotwave(fig, 711, NORMAL(data))
+plotwave(fig, 712, NRZ(data))
+plotwave(fig, 713, NRZ2(data))
+plotwave(fig, 714, RZ(data))
+plotwave(fig, 715, RZ2(data))
+plotwave(fig, 716, AMI(data))
+plotwave(fig, 717, MAN(data))
+plt.show()
+
+"""
+ax = fig.add_subplot(712).plot(range(width * 4), NRZ(data).flatten())
 ax = fig.add_subplot(713).plot(range(width * 4), NRZ2(data).flatten())
 ax = fig.add_subplot(714).plot(range(width * 4), RZ(data).flatten())
 ax = fig.add_subplot(715).plot(range(width * 4), RZ2(data).flatten())
 ax = fig.add_subplot(716).plot(range(width * 4), AMI(data).flatten())
 ax = fig.add_subplot(717).plot(range(width * 4), MAN(data).flatten())
-ax1.grid(True, 'both', 'both')
-plt.show()
+"""
 
+"""
 graphdata(NORMAL(data[1:24]))
 graphdata(NRZ(data[1:24]))
 graphdata(NRZ2(data[1:24]))
@@ -77,5 +96,6 @@ graphdata(RZ(data[1:24]))
 graphdata(RZ2(data[1:24]))
 graphdata(AMI(data[1:24]))
 graphdata(MAN(data[1:24]))
+"""
 
 #print(np.reshape([1,2,3,4], (-1, 1) ) * [2, 3])
