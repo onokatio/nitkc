3411b08131c4c40614efb9acd2c250bca5a470b7
onokatio <onokatio@gmail.com>
2020-07-09 11:59:16 +0900

    use lock

--- 5/distributed/8/slide.c
+++ 5/distributed/8/slide.c
@@ -4,11 +4,16 @@
 
 int main(void){
 	int i,s = 0;
+	omp_lock_t lock;
+	omp_init_lock(&lock);
+
 	omp_set_num_threads(2);
+
 	#pragma omp parallel for
-	for (i = 0; i < 10; i++){
-		usleep(random() % 100000);
+	for (i = 0; i < 100000; i++){
+		omp_set_lock(&lock);
 		s++;
+		omp_unset_lock(&lock);
 	}
 	printf("%d\n",s);
 }
